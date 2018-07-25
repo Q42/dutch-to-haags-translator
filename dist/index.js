313a829324ea206cc4a3f3dc8855e3c494fbe97c
Bob van Oorschot <bob@q42.nl>
2018-07-25 17:07:44 +0200

    old files

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("source-map-support/register");
-const { showHits } = require('./translator');
-showHits('Oh oh Den Haag, mooie stad achter de duinen!');
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import "source-map-support/register";
-
-const {showHits} = require('./translator');
-
-showHits('Oh oh Den Haag, mooie stad achter de duinen!');
